03521f01115a8f3244a1164f936c34c6bdba367c
Jonathan Garcia-Rovetta <jonathangarcia-rovetta@Jonathans-MacBook-Pro.local>
2017-10-22 00:42:35 -0700

    first commit

---
+++ Assets/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	Rigidbody2D rb;
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce(transform.up * 10f);
+	}
+
+	void FixedUpdate () {
+		
+		float move = Input.GetAxis ("Horizontal");
+
+
+
+	}
+
+}
---
+++ Assets/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+	// Use this for initialization
+	Rigidbody2D rb;
+	private float maxSpeed=10f;
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+
+		float move = Input.GetAxis ("Horizontal");
+		rb.velocity = new Vector2 (move*maxSpeed, rb.velocity.y);
+
+
+	}
+
+}
