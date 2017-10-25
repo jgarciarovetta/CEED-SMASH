fe55a5864988c1cc81da3cf52add5be100fccd76
Jonathan Garcia-Rovetta <jonathangarcia-rovetta@Jonathans-MacBook-Pro.local>
2017-10-24 23:58:31 -0700

    some stuff

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -2,25 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class Character : MonoBehaviour {
 
 	// Use this for initialization
 	Rigidbody2D rb;
+	float maxSpeed = 10f;
+	float jumpForce = 150f;
+	bool landed = true;
+	float distToGround;
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		distToGround = gameObject.GetComponent<Collider2D> ().bounds.extents.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		rb.AddForce(transform.up * 10f);
+		
 	}
 
 	void FixedUpdate () {
 		
-		float move = Input.GetAxis ("Horizontal");
+		float move = Input.GetAxisRaw("Horizontal");
+		rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
+
+		if ((Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow)) && landed==true) {
+			
+			rb.AddForce (transform.up * jumpForce);
+			landed=false;
+		}	
 
 
 
+	}
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		Debug.Log (coll.gameObject.tag);
+			//Debug.Log ("WORKS");
+		if(coll.gameObject.tag == "ground")
+		{
+			Debug.Log ("WORKS");
+			landed = true;
+		}
 	}
 
 }
